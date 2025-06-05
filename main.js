c0a3d5b960a4941806e8a51e99b21ecb67ffa8fb
vanna567 <112666892+vanna567@users.noreply.github.com>
2025-06-05 17:48:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let latitude, longitude, destino;
+let latitude, longitude, destino; //Localización de la Ciudad de México19.622342, -99.237675
 
 $(document).ready(function (){
-    alert("Permite tener acceso a tu hubicación")
+    alert("Por favor, permite que el dispositivo acceda a tu ubicación")
     initGeolocation();
 })
 
-//Enviar coordenadas de navegación a la siguiente página por medio del boton navegar
+//enviar coordenadas de navegacion a la sig pagina por medio del boton NAVEGAR
 $(function(){
     $("#navigate-button").click(function(){
         window.location.href = `ar_nav.html?source=${latitude};${longitude}&destino=${destino.lat};${destino.lng}`
@@ -15,61 +15,58 @@ $(function(){
 function initGeolocation(){
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(muestralo)
-
+        console.log(muestralo)
     }else{
         alert("Lo sentimos, tu navegador no es compatible")
     }
 }
+
 function muestralo(position){
     console.log(position)
     longitude = position.coords.longitude;
     latitude = position.coords.latitude;
 
-    //inicializamos Mapbox
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY2FseXBzc28wMSIsImEiOiJjbWFyZnBpa3QwYXg3MnJweXdvZHFlaGYzIn0.oI1ytE7WFodTW6X-Dl62Iw'
+    //Inicializamos Mapbox
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2FseXBzc28wMSIsImEiOiJjbWFnMTdwd3owOHVtMmxweXh3bHlvdjAwIn0.cGGP7feYVXdukWUt1zBccw';
 
     var map = new mapboxgl.Map({
-        container: 'map', //sección donde se muestra el mapa
-        style: 'mapbox://styles/mapbox/streets-v12', // estilo del mapa
-        center: [longitude, latitude], //coordenadas del centro del mapa
-        zoom:16 //nivel de zoom inicial (min 0 max 22)
+    container : 'map',// sección donde se muestra el mapa
+    style : 'mapbox://styles/mapbox/streets-v12',// estilo del mapa
+    center: [longitude, latitude],// coordenadas del centro del mapa
+    zoom:16 // nivel de zoom inicial  min 0 max 22
     });
 
-    // Añadir control de Geolocalización el Usuario
+    // añadir control de localización del ususario
     map.addControl(
         new mapboxgl.GeolocateControl({
-           positionOptions:{
-             enableHighAccuracy: true
-            },
-             trackUserLocation: true
-        }),
+         positionOptions:{
+            enableHighAccuracy: true
+         },
+         trackUserLocation: true
+        })
     );
 
-    // Añadir control de navegación de A a B
+    //añadir control de direccion de A a B
     map.addControl(
-       new MapboxDirections ({
-        accesToken: mapboxgl.accesToken,
-        unit: 'metric',
-        controls:{
-            inputs: true,
-            instructions: true,
-            waypoints: true,
-            geocoder: true
-        }
-    }),
-    'top-left'
-  );
+        new MapboxDirections({
+            accessToken: mapboxgl.accessToken,
+            unit: 'metric',
+            controls:{
+                inputs: true,
+                instructions: true,
+                waypoints: true,
+                geocoder: true
+            }
+        }),
+        'top-left'
+    );
 
     //función chismoso .on('click') para obtener lng y lat
     map.on('click', function(e){
-        // function para cueando el evento se convierte en acción
-        console.log(e)
+        console.log(e);
         destino = e.lngLat
     })
 }
-
-
-
 
 
 
